91a12449665fe1ac9d8c87a4484701a836653cd6
Brandy Kundenga <55144997+BrandyKun@users.noreply.github.com>
2022-12-13 06:36:58 +0000

    commit everythign so far

---
+++ sketches/sketch-01.js
@@ -0,0 +1,33 @@
+const canvasSketch = require('canvas-sketch');
+
+const settings = {
+  dimensions: [ 600, 600 ]
+};
+
+const sketch = () => {
+  return ({ context, width, height }) => {
+    context.fillStyle = 'white';
+    context.fillRect(0, 0, width, height);
+
+    for (let i = 0; i < 5; i++) {
+      for (let j = 0; j < 5; j++) {
+        let w = 60;
+        let h = 60;
+        let gap = 20;
+        let x = 100 + (w + gap) * i;
+        let y = 100 + (h + gap) * j;
+
+        context.beginPath();
+        context.rect(x, y, w, h);
+        context.stroke();
+        if (Math.random() > 0.5) {
+          context.beginPath();
+          context.rect(x + 8, y + 8, w , h);
+          context.stroke();
+        }
+      }
+    }
+  };
+};
+
+canvasSketch(sketch, settings);
